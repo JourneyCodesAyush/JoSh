05cd7db94ea59191efb493763670b7fc4593ec82
JourneyCodesAyush <mithilesh71170@gmail.com>
2025-08-10 14:56:52 +0530

    feat(script): add message and command line display

--- script.js
+++ script.js
@@ -94,6 +94,65 @@ const fileSystem = {
     },
 };
 
+
+/**
+ * Adds a new command line interface section to the terminal and sets up input listener.
+ */
+function addNewCommandLine() {
+
+    const terminalBody = document.querySelector(".terminalBody");
+    const userShellInput = document.createElement("div");
+    userShellInput.className = "userShell";
+
+    const displayPath =
+        currentPath.length > 0 ? `/${currentPath.join("/")}` : "/";
+
+    userShellInput.innerHTML = `<label for="userCommand" class="title">
+                    <div class="userInfo">visitor@visitor0943: ~<span class="directory">${displayPath}</span> $</div>
+                </label>
+                <div class="userInput">
+                    <input class="userCommand" name="userCommand" type="text" size="51" autofocus>
+                </div>`;
+
+    terminalBody.appendChild(userShellInput);
+
+    userShellInput.querySelector(".userCommand").focus();
+    userShellInput
+        .querySelector(".userCommand")
+        .addEventListener("keypress", (e) => {
+            if (e.key === "Enter") {
+                const command = e.target.value;
+                argParse(command);
+            }
+        });
+
+    terminalBody.scrollTop = terminalBody.scrollHeight;
+}
+
+/**
+ * Adds a message output to the terminal.
+ * 
+ * @param {string} message - The message to display.
+ * @param {boolean} [success=true] - Whether the message indicates success or not.
+ */
+function addMessage(message, success = true) {
+    Array.from(document.getElementsByTagName("input")).forEach(input => {
+        input.disabled = true;
+    });
+    if (success) {
+
+        const terminalBody = document.querySelector(".terminalBody");
+        const userShellOutput = document.createElement("div");
+        userShellOutput.className = "userShell";
+        userShellOutput.innerHTML = `                
+        <div class="userOutput"></div>`;
+
+        userShellOutput.querySelector(".userOutput").textContent = message;
+        terminalBody.appendChild(userShellOutput);
+    }
+}
+
+
 /**
  * Parses the user input command and triggers the appropriate handler (cd, ls, etc.).
  *
