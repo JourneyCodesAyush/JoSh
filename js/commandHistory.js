20e2608fbdbdd6f39d397a256eb7f7c33c4ee2bb
JourneyCodesAyush <mithilesh71170@gmail.com>
2025-09-10 19:11:25 +0530

    refactor(js/commandHistory): add commandHistory array to store the commands

---
+++ js/commandHistory.js
@@ -0,0 +1,15 @@
+// js/commandHistory.js
+
+/**
+ * @module js/commandHistory
+ * 
+ * Stores the history of executed terminal commands.
+ * Each entry is an array of strings representing command and its arguments.
+ * Example: [ 'cd', 'projects' ]
+ * 
+ * @exports commandHistory
+ */
+
+const commandHistory = [];
+
+export { commandHistory };
